0926a340b4694a4fd5137874a824fb57431cb828
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-10-21 19:44:11 -0700

    - add index.js  - update App.js  - add new tests on App.test.js

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -4,6 +4,21 @@ import App from './App';
 
 const app = shallow(<App />)
 
+// this test will check if everything was rendered correctly
 it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
+   expect(app).toMatchSnapshot();
 });
+
+// this will check if the state is empty or not.
+it('initializes the `state` with an empty list of gifts', () => {
+   expect(app.state().gifts).toEqual([]);
+});
+
+// this will check if the button was clicked and also if a new gift was added
+// when the button was clicked
+it('adds a new gifts `state` when clicking the `add gift` button', () => {
+   // this will simulate the button.
+   app.find('.btn-add').simulate('click');
+
+   expect(app.state().gifts).toEqual([{id: 1}])
+});   
